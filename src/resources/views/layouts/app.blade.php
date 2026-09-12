3100ff6aba9fbd73189399a3376cf619ae76dfc2
ym_work <256305102+ymwork-dev@users.noreply.github.com>
2026-09-12 18:05:57 +0900

    ログイン時のヘッダー修正

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -44,17 +44,17 @@
                                 <button type="submit" class="header__nav-link header__logout-btn">ログアウト</button>
                             </form>
                         </li>
+                        <li class="header__nav-item">
+                            <a href="/mypage" class="header__nav-link">マイページ</a>
+                        </li>
+                        <li class="header__nav-item">
+                            <a href="/sell" class="header__sell-btn">出品</a>
+                        </li>
                     @else
                         <li class="header__nav-item">
                             <a href="/login" class="header__nav-link">ログイン</a>
                         </li>
                     @endif
-                    <li class="header__nav-item">
-                        <a href="/mypage" class="header__nav-link">マイページ</a>
-                    </li>
-                    <li class="header__nav-item">
-                        <a href="/sell" class="header__sell-btn">出品</a>
-                    </li>
                 </ul>
             </nav>
         </div>
